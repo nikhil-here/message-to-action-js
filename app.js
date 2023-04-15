63625900c6283028e55295e79f0fba8afbc60089
nikhil-mandlik <54818560+nikhil-here@users.noreply.github.com>
2023-04-16 02:15:38 +0530

    removing tasks and rules from user prompt

--- app.js
+++ app.js
@@ -2,10 +2,8 @@ const FineTuningDataSet = require("./models/FineTuningDataSet");
 const fs = require("fs");
 const tests = require("./data/data");
 
+const task = `You are api middleware, your responsibility is to convert user raw query to json object which we can use to perform a database operation,basically database lookup language, try to be creative with understanding user query which can be in different languages, we want to perform READ database operation based on the above user query, map the exact user action from the above user query and also follow following rules while giving response`;
 
-const jsonPattern = "###"
-
-const task = `we want to perform READ database operation based on the above user query, map the exact user action from the above user query and also follow following rules while giving response`;
 const rules = [
   'response "should" be in the form of JSON only. don\'t add any additional information or explanation in the response.',
   'response JSON should contain only following fields "userAction" and "extraParams".',
@@ -17,23 +15,31 @@ const rules = [
   'add the "query" field inside nested "extraParams" JSON, "query" field should contain any food item name, category or description mention in the user query, if you are not able to find any specific food item, category or description in the user query, keep "query" field as null',
   'add the "item" field inside nested "extraParams", "item" field will contain specific food item mention in the user query, if there are no food item mention in the user query then keep the "item" field json value as null',
   'add the "category" field inside nested "extraParams", based on user query try to find the "category" of food item user is trying to request, if there are no specific food category is mention in the user query then keep "category" json field value as null',
-  `add ${jsonPattern} at the beginning and end of the response JSON`,
 ];
 var ruleString = "";
 rules.forEach((rule) => {
   ruleString = ruleString + `\n${rule}`;
 });
 
-tests.forEach((test, index) => {
-  const promptString = `User Query: ${test.query}\nTask: ${task}\nRules:${ruleString} ->`;
-  const completionString = ` ${jsonPattern}${JSON.stringify(
-    test.response
-  )}${jsonPattern}`;
+let date = new Date();
+
+const year = date.getFullYear();
+const month = date.getMonth()
+const hour = date.getHours()
+const dateString = date.getDate()
+const minutes = date.getMinutes()
+const seconds = date.getSeconds()
+
+const fileName = `dataset-${year}-${month}-${dateString}-${hour}-${minutes}-${seconds}.jsonl`;
+
+tests.forEach((test) => {
+  const promptString = `${test.query}\n\n###\n\n`;
+  const completionString = ` ${JSON.stringify(test.response)} END`;
 
   const model = new FineTuningDataSet(promptString, completionString);
   const result = `${JSON.stringify(model)}\n`;
 
-  fs.appendFile("dataset.jsonl", result, (err) => {
+  fs.appendFile(fileName, result, (err) => {
     if (err) {
       console.error(err);
     }
